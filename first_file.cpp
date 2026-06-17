acd673324041b9e77fcf54cde283ce0ef7e09fb6
hieu <hieuquedeptrai@gmail.com>
2026-06-17 16:45:48 +0700

    Commiting files after merging

--- first_file.cpp
+++ first_file.cpp
@@ -8,5 +8,14 @@
 
 int main(){
     std::cout << "I love Git" << std::endl;
+    
+    
+    
+    // This is branch for work actually change
+    std::cout<< "I love merging and branching!" << std::endl;
+
+
+
     return 1;
 }
+
